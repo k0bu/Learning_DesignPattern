9fc20b2f30a7518bd24b66054c650cfb35817c23
こぶちゃ <30616294+k0bu@users.noreply.github.com>
2018-11-15 20:57:39 +0900

    TemplateMethod

---
+++ TemplateMethod/Main.java
@@ -0,0 +1,14 @@
+public class Main{
+    public static void main(String[] args) {
+        AbstractDisplay charDsp = new CharDisplay('H');
+        AbstractDisplay strDsp = new StringDisplay("Hello World!");
+        AbstractDisplay strDsp2 = new StringDisplay("こんにちは");
+
+        charDsp.display();
+        System.out.println("");
+        strDsp.display();
+        System.out.println("");
+        strDsp2.display();
+
+    }
+}
